2d2bc13003543f7c6a3f2ffe50f217ed34c6e8bd
rlcunha <rlcunha@outlook.com.br>
2025-05-05 17:46:06 -0300

    ajuste de cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -136,6 +136,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://appteste.mibitech.com.br",
+        "https://apirest.mibitech.com.br",
         "http://localhost:3000",
         "http://127.0.0.1:3000"
     ],
